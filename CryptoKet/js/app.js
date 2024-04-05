006b77d7d72567e371711144296bd57ac2772dca
dashok29 <150663098+dashok29@users.noreply.github.com>
2024-04-05 10:12:54 +0300

    Delete CryptoKet directory

--- CryptoKet/js/app.js
+++
@@ -1,9 +0,0 @@
-
-    let swiper = new Swiper(".mySwiper", {
-      slidesPerView: 5,
-      spaceBetween: 30,
-      navigation: {
-        nextEl: ".slide-arrow-right",
-        prevEl: ".slide-arrow-left",
-      },
-    });
